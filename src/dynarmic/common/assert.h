cc3d0c381abfdfea86fb097e9cea040321c9d337
lizzie <lizzie@eden-emu.dev>
2026-07-22 20:24:15 +0000

    add missing hdrs2

---
+++ src/dynarmic/common/assert.h
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// SPDX-FileCopyrightText: 2013 Dolphin Emulator Project
+// SPDX-FileCopyrightText: 2014 Citra Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <cassert>
+
+#define ASSERT(_a_) assert(_a_)
+#define DEBUG_ASSERT(_a_) assert(_a_)
